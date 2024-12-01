abce6ff548dfcb403ed143c1550ef21383e35182
sumitkumarpradhan <sumitpradhan.kumar@gmail.com>
2024-12-01 11:33:45 +0530

    initalization

---
+++ src/main/java/com/java/wikisearch/WikiSearchApplication.java
@@ -0,0 +1,13 @@
+package com.java.wikisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/wikisearch/WikiSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.wikisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
